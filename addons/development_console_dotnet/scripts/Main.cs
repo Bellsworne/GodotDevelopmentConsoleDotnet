f2014604aab71aee2c5df83e1ad4b56078601856
Spudnut2000 <chrisbell@hexstudios.co>
2024-04-30 23:01:34 -0500

    Removed broken auto input map functionality

--- addons/development_console_dotnet/scripts/Main.cs
+++ addons/development_console_dotnet/scripts/Main.cs
@@ -9,27 +9,10 @@ public partial class Main : EditorPlugin
     public override void _EnablePlugin()
     {
         AddAutoloadSingleton("GameConsole", GameConsoleUiPath);
-
-        
-        InputEventKey _inputEventKey = new();
-        _inputEventKey.Keycode = Key.Enter;
-        InputMap.AddAction("accept");
-        InputMap.ActionAddEvent("accept", _inputEventKey);
-        
-        // InputEventKey _inputEventKey = new();
-        // _inputEventKey.Keycode = Key.Enter;
-        // InputMap.AddAction("accept");
-        // InputMap.ActionAddEvent("accept", _inputEventKey);
-        // _inputEventKey.Keycode = Key.Asciitilde;
-        // InputMap.AddAction("console");
-        // InputMap.ActionAddEvent("console", _inputEventKey);
     }
     
     public override void _DisablePlugin()
     {
         RemoveAutoloadSingleton("GameConsole");
-        
-        InputMap.EraseAction("accept");
-        InputMap.EraseAction("console");
     }
 }
